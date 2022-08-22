0a8eb3072091d4055577860aa521b26e033f73ac
Matteo Susanna <matteo2784@gmail.com>
2022-08-22 14:20:38 +0200

    fine

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 var app = new Vue({
     el: '#app',
     data: {
-        mailCasuale: null,
+        mailCasuale: [],
     },
     mounted(){
-      
-      axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then(mail => {
-          this.mailCasuale = mail.data.response
-        })
+        for(let i = 0; i < 10; i++){
+          axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(mail => {
+              this.mailCasuale.push(mail.data.response) 
+
+              console.log(this.mailCasuale)
+            }) 
+        }    
     },
   })
